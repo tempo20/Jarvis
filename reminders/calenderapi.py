7ae7c07196398079abdd2579f4d8c41515ee437d
tempo20 <kwoktristan420@gmail.com>
2024-08-10 15:47:54 +0800

    calendar api file

--- reminders/calenderapi.py
+++ reminders/calenderapi.py
@@ -18,15 +18,15 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 credentials_file_path = config.get('General', 'file_path')
 
-# %%
-def main():
+#%%
+def get_creds():
     creds = None
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json")
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refres(Request())
+            creds.refresh(Request())
 
         else:
             flow = InstalledAppFlow.from_client_secrets_file(credentials_file_path, SCOPES)
@@ -34,24 +34,24 @@ def main():
 
             with open("token.json", "w") as token:
                 token.write(creds.to_json())
-    
+    return creds
+#%%
+def get_events():
+    creds = get_creds()
     try:
-       service = build('calendar', 'v3', credentials = creds)
-       now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-       print("Getting the upcoming 10 events")
-       event_result = service.events().list(calendarId = 'primary', timeMin = now, maxResults = 5, singleEvents = True, orderBy = 'startTime').execute()
-       events = event_result.get('items', [])
-       if not events:
-           print('No upcoming events found!')
-           return
-       for event in events:
-           start = event['start'].get('dateTime', event['start'].get('date'))
-           print(start, event['summary'])
-        
+        service = build('calendar', 'v3', credentials = creds)
+        now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+        print("Getting the upcoming 10 events")
+        event_result = service.events().list(calendarId = 'primary', timeMin = now, maxResults = 5, singleEvents = True, orderBy = 'startTime').execute()
+        events = event_result.get('items', [])
+        if not events:
+            print('No upcoming events found!')
+            return
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
     except HttpError as error:
         print('an error has occured:', error)
 
 # %%
-if __name__ == '__main__':
-    main()
-# %%
+
