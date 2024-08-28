bc9967c9231bf851c8b28c4274e2774340195e33
Tristan Kwok <tristan@Tristans-MacBook-Air.local>
2024-08-28 21:02:25 +0800

    changed summary parameters

--- models/FalconsAI/model.py
+++ models/FalconsAI/model.py
@@ -8,7 +8,7 @@ max_length = 200
 
 def get_summary(text):
     inputs = tokenizer(text, return_tensors="pt",
-                       max_length=512, truncation=True)
+                       max_length=2048, truncation=True)
     summary_ids = model.generate(inputs["input_ids"], max_length=max_length, min_length=30, length_penalty=3.0,
                                  num_beams=3, temperature=0.2, repetition_penalty=2.0, early_stopping=False, do_sample=True)
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
